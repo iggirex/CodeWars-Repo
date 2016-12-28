8b8d22c85c63040d1d8ba417a2e0d74156b79951
Ignacio (iggi) Aguerrevere Bazo <iggirex@gmail.com>
2016-12-28 13:24:14 -0700

    Update needleInHaystack.js

--- needleInHaystack.js
+++ needleInHaystack.js
@@ -5,3 +5,9 @@ function findNeedle(haystack) {
   }
   return "found the needle at position " + index
 }
+
+BETTER-------------------------
+
+function findNeedle(haystack) {
+  return "found the needle at position " + haystack.indexOf("needle")
+}
