92f707cd7f7a1bd19bb5d08d37013dfeac276eb6
Ignacio (iggi) Aguerrevere Bazo <iggirex@gmail.com>
2016-12-29 13:08:41 -0700

    Update findMinMax.js

--- findMinMax.js
+++ findMinMax.js
@@ -13,3 +13,28 @@ var max = function(list){
     })
     return most;
 }
+---------------------------------->
+    
+    
+var min = function(list){
+    list.sort((a, b) => a - b)
+    return list[0];
+}
+
+var max = function(list){
+    list.sort((a, b) => a - b)
+    return list[list.length - 1];
+}
+
+--------------------------------->
+    
+    
+var min = function(list){
+    list.sort((a, b) => a -b)
+    return list[0];
+}
+
+var max = function(list){
+    list.sort((a, b) => b - a)
+    return list[0];
+}
