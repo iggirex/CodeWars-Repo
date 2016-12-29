d86a6756499b5a4d4d8facae19232a1585c4ee82
Ignacio (iggi) Aguerrevere Bazo <iggirex@gmail.com>
2016-12-29 12:09:20 -0700

    Create wordSmash.js

---
+++ wordSmash.js
@@ -0,0 +1,8 @@
+function smash (words) {
+    "use strict";
+    var sentence = "";
+    words.forEach(function(curr, i, arr){
+      arr.indexOf(curr) === arr.length - 1 ? sentence += curr : sentence += curr + " ";
+    })
+    return sentence;
+};
