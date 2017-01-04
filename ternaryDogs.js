f6d89a11e142888027bb2cd686b856af107f5d7a
Ignacio (iggi) Aguerrevere Bazo <iggirex@gmail.com>
2017-01-03 18:43:21 -0700

    Update ternaryDogs.js

--- ternaryDogs.js
+++ ternaryDogs.js
@@ -14,3 +14,9 @@ Complete function saleHotdogs, function accept 1 parameters:n, n is the number o
 function saleHotdogs(n){
   return n < 5 ? n * 100 : n < 10 ? n * 95 : n * 90;
 }
+
+------------------->
+
+function saleHotdogs(n){
+  return n * (n < 5 ? 100 : n < 10 ? 95 : 90;
+}
