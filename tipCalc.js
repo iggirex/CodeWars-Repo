e880cc39ad495aa59809479b72efbc9f123bdc0f
Ignacio (iggi) Aguerrevere Bazo <iggirex@gmail.com>
2017-01-04 14:46:44 -0700

    Update tipCalc.js

--- tipCalc.js
+++ tipCalc.js
@@ -22,3 +22,19 @@ function calculateTip(amount, rating) {
   default : return "Rating not recognised";
   }
 }
+
+---------------------->
+  
+  const TIPS = {
+  "terrible": 0.0,
+  "poor": 0.05,
+  "good": 0.1,
+  "great": 0.15,
+  "excellent": 0.2
+};
+
+const calculateTip = (amount, rating) => {
+  rating = rating.toLowerCase();
+  
+  return rating in TIPS ? Math.ceil(TIPS[rating] * amount) : "Rating not recognised";
+};
